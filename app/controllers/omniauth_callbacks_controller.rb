c0062ed4f902ab7895dbdbe2d1b52a605950f899
Jeremy Raines <jraines@gmail.com>
2015-07-20 14:54:49 -0400

    Register or login user via GitHub

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,15 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def github
+    @user = User.find_for_github_oauth(user_params)
+
+    flash[:notice] = "Welcome! #{@user.email}"
+    sign_in(@user)
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    request.env["omniauth.auth"].permit!(:provider, :uid, :info)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
          :validatable, :omniauthable, omniauth_providers: [:github]
+
+  def self.find_for_github_oauth(auth)
+    #when we slice auth we get:
+    #{:provider => 'github', :uid => 'some_uid'}
+
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
 end
