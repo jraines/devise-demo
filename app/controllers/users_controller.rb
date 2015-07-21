8c18b7c9dd85f61efb23666ff7bbcac5d7a7464b
Jeremy Raines <jraines@gmail.com>
2015-07-21 18:00:01 -0400

    Get all repos in controller by paginating throug api response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,13 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by_username params[:id]
+    @user  = User.find_by_username params[:id]
+    @repos = []
+    Github.new.
+           repos.
+           list(user: @user.username).
+           each_page{|p|
+             p.each{|r| @repos << r }
+           }
   end
 end
