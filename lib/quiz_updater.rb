0df1b6b5819378d28c64db032665fa7ca612da46
Jeremy Raines <jraines@gmail.com>
2015-08-05 14:57:14 -0400

    Quiz updater pass

--- lib/quiz_updater.rb
+++ lib/quiz_updater.rb
@@ -1,23 +1,40 @@
 class QuizUpdater
 
-  attr_accessor :quiz
+  attr_accessor :quiz, :quiz_params
 
   def initialize(quiz)
     @quiz = quiz
   end
 
   def update(quiz_params)
+    @quiz_params = quiz_params
 
-    quiz_params[:quiz][:questions_attributes].each do |key, val|
-      id = val.delete(:id)
+    delete_removed_questions
+
+    quiz_params[:quiz][:questions_attributes].each do |_, qa|
+      id = qa.delete(:id)
       if id.blank?
-        quiz.questions.create!(val)
+        quiz.questions.create!(qa)
       else
         question = quiz.questions.find(id)
-        question.update(val)
+        question.update(qa)
       end
     end
 
     quiz
   end
+
+  private
+
+  def delete_removed_questions
+    quiz_question_ids = quiz.questions.pluck :id
+
+    submitted_question_ids = quiz_params[:quiz][:questions_attributes].map do |_, qa|
+      qa[:id]
+    end.compact
+
+    ids_of_questions_to_delete = quiz_question_ids - submitted_question_ids
+    quiz.questions.where(id: ids_of_questions_to_delete).destroy_all
+  end
+
 end
--- spec/lib/quiz_updater_spec.rb
+++ spec/lib/quiz_updater_spec.rb
@@ -10,15 +10,19 @@ describe QuizUpdater do
       name: 'New quiz',
       questions_attributes: {
         '0' => {
+          id: quiz.questions.first.id,
+          body: 'MyText' ,
+          answers_attributes: {
+            '0' => { body: 'Sure',
+                     correct: false }
+          }},
+
+        '1' => {
           body: 'To be or nah?' ,
           answers_attributes: {
             '0' => { body: 'Sure',
-                     correct: true }
-          }
-        }
-      }
-    }}
-  }
+                     correct: true }}}
+      }}}}
 
   before do
     @updater = QuizUpdater.new(quiz)
@@ -28,6 +32,11 @@ describe QuizUpdater do
     it "adds new questions, based on params that don't match existing ones" do
       changed_quiz = @updater.update(quiz_params)
       expect(changed_quiz.questions.count).to eq(2)
+    end
+
+    it "remove questions that are not present in the params" do
+      changed_quiz = @updater.update(quiz_params)
+      expect(changed_quiz.questions.map(&:body)).to include('To be or nah?')
     end
   end
 
