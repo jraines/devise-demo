94124fb3d05eddb62d674e4979ab0f5ff499ad7c
Jeremy Raines <jraines@gmail.com>
2015-08-11 14:19:53 -0400

    Start on Dribble API wrapper

---
+++ app/helpers/dribble_shots_helper.rb
@@ -0,0 +1,2 @@
+module DribbleShotsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
 
+  get '/dribble_shots' => 'dribble_shots#index'
+
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
